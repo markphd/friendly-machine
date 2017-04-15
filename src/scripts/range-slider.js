37bedeb0df78b0fc90d1dd9a273acbd91c81623b
Mark Philip Dawal <radical.mark@gmail.com>
2017-04-15 22:34:59 +0400

    ⟜started working on image contrast slider

--- src/scripts/range-slider.js
+++ src/scripts/range-slider.js
@@ -1,5 +1,17 @@
+
+
+var imageContrast = document.getElementById('image-contrast');
+
 var perforationSize = document.getElementById('perforation-size');
 var perforationIntensity = document.getElementById('perforation-intensity');
+
+noUiSlider.create(imageContrast, {
+  start: [ 4000 ],
+    range: {
+      'min': [  2000 ],
+      'max': [ 10000 ]
+    }
+});
 
 noUiSlider.create(perforationSize, {
   start: [20,80],
