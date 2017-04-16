528793ab82771aea9229d4ae859d66559abee210
Mark Philip Dawal <radical.mark@gmail.com>
2017-04-16 22:55:34 +0400

    ⟜implemented side by side range for wall width and height size

--- src/scripts/range-slider.js
+++ src/scripts/range-slider.js
@@ -1,9 +1,27 @@
 
-
+var wallWidth = document.getElementById('wall-width');
+var wallHeight = document.getElementById('wall-height');
 var imageContrast = document.getElementById('image-contrast');
-
+var gridSize = document.getElementById('grid-size');
 var perforationSize = document.getElementById('perforation-size');
 var perforationIntensity = document.getElementById('perforation-intensity');
+
+noUiSlider.create(wallWidth, {
+  start: [ 4000 ],
+    range: {
+      'min': [  2000 ],
+      'max': [ 10000 ]
+    }
+});
+
+noUiSlider.create(wallHeight, {
+  start: [ 4000 ],
+    range: {
+      'min': [  2000 ],
+      'max': [ 10000 ]
+    }
+});
+
 
 noUiSlider.create(imageContrast, {
   start: [ 4000 ],
@@ -11,6 +29,15 @@ noUiSlider.create(imageContrast, {
       'min': [  2000 ],
       'max': [ 10000 ]
     }
+});
+
+noUiSlider.create(gridSize, {
+  start: [20,80],
+  connect: true,
+  range: {
+    min: 1,
+    max: 200
+  }
 });
 
 noUiSlider.create(perforationSize, {
